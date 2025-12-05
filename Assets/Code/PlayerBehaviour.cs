4e89aa2383a26ff913da7cf09eb8daca5ff0b47f
Adam Wielogórski <71398159+Khangineer@users.noreply.github.com>
2025-12-05 12:13:58 +0100

    Character movement done

---
+++ Assets/Code/PlayerBehaviour.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private enum State
+    {
+        Left,
+        Right
+    }
+
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private GameObject _kiwiProjectile;
+
+    private Rigidbody2D _rigidbody;
+    private Animator _animator;
+    private Vector2 _moveInput;
+    private Vector2 _velocity;
+    private SpriteRenderer _spriteRenderer;
+
+    private static State _state;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        Vector2 move = new Vector2(_moveInput.x, 0);
+        _rigidbody.linearVelocity = move * _speed;
+        HandleKiwiShooting();
+        UpdateAnimation(move);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void HandleKiwiShooting()
+    {
+
+    }
+
+    private void UpdateAnimation(Vector2 move)
+    {
+        _animator.SetFloat("MoveX", move.x);
+        _animator.SetBool("IsMoving", Mathf.Abs(move.x) > 0.01f);
+
+        if (move.x < 0)
+        {
+            _spriteRenderer.flipX = true;
+            _animator.SetInteger("MoveXINT", 1);
+        }
+        else if (move.x == 0)
+        {
+            _animator.SetInteger("MoveXINT", 0);
+        }
+        else
+        {
+            _spriteRenderer.flipX = false;
+            _animator.SetInteger("MoveXINT", 1);
+        }
+    }
+}
